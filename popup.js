4d57d7fefd326d4ce1fc6cfe8f406b6a8b38ce5b
ohlala779 <regdomainkris@gmail.com>
2025-08-12 06:26:25 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+<script>
 (function() {
   function loadScript(url) {
     var script = document.createElement('script');
@@ -7,6 +8,24 @@
     document.head.appendChild(script);
   }
 
-  loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1792720/e9692f9f.js'); 
-  loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1767634/0b79b57a.js'); 
+  document.addEventListener('click', function () {
+    var pop = window.open('about:blank', '_blank');
+    if (pop) {
+      pop.blur(); // đưa cửa sổ ra sau
+      window.focus();
+      // Tải script trong popunder
+      pop.document.write('<!DOCTYPE html><html><head></head><body></body></html>');
+      pop.document.close();
+      pop.onload = function() {
+        var s1 = pop.document.createElement('script');
+        s1.src = '//diagramjawlineunhappy.com/t/9/fret/meow4/1792720/e9692f9f.js';
+        pop.document.head.appendChild(s1);
+
+        var s2 = pop.document.createElement('script');
+        s2.src = '//diagramjawlineunhappy.com/t/9/fret/meow4/1767634/0b79b57a.js';
+        pop.document.head.appendChild(s2);
+      };
+    }
+  }, { once: true });
 })();
+</script>
