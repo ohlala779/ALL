ebf769230790bf05d023f231c6f4b4be22599fb9
ohlala779 <regdomainkris@gmail.com>
2025-08-12 06:32:27 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,31 +1,32 @@
 <script>
 (function() {
-  function loadScript(url) {
-    var script = document.createElement('script');
-    script.setAttribute('data-cfasync', 'false');
-    script.type = 'text/javascript';
-    script.src = url;
-    document.head.appendChild(script);
-  }
+  var opened = false;
 
-  document.addEventListener('click', function () {
+  function openPopunder() {
+    if (opened) return;
+    opened = true;
+
     var pop = window.open('about:blank', '_blank');
     if (pop) {
-      pop.blur(); // đưa cửa sổ ra sau
-      window.focus();
-      // Tải script trong popunder
       pop.document.write('<!DOCTYPE html><html><head></head><body></body></html>');
       pop.document.close();
-      pop.onload = function() {
-        var s1 = pop.document.createElement('script');
-        s1.src = '//diagramjawlineunhappy.com/t/9/fret/meow4/1792720/e9692f9f.js';
-        pop.document.head.appendChild(s1);
 
-        var s2 = pop.document.createElement('script');
-        s2.src = '//diagramjawlineunhappy.com/t/9/fret/meow4/1767634/0b79b57a.js';
-        pop.document.head.appendChild(s2);
-      };
+      // Đưa popunder ra sau
+      pop.blur();
+      window.focus();
+
+      // Tải script vào popunder
+      var s1 = pop.document.createElement('script');
+      s1.src = '//diagramjawlineunhappy.com/t/9/fret/meow4/1792720/e9692f9f.js';
+      pop.document.head.appendChild(s1);
+
+      var s2 = pop.document.createElement('script');
+      s2.src = '//diagramjawlineunhappy.com/t/9/fret/meow4/1767634/0b79b57a.js';
+      pop.document.head.appendChild(s2);
     }
-  }, { once: true });
+  }
+
+  // Lắng nghe click ở bất kỳ đâu
+  document.addEventListener('click', openPopunder);
 })();
 </script>
