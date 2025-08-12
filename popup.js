1de1dc2f1db081e69b1dcc1765cb363e16feeb08
ohlala779 <regdomainkris@gmail.com>
2025-08-12 06:37:10 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,37 +1,12 @@
-<script>
 (function() {
-  function loadScript(targetDoc, url) {
-    var script = targetDoc.createElement('script');
+  function loadScript(url) {
+    var script = document.createElement('script');
     script.setAttribute('data-cfasync', 'false');
     script.type = 'text/javascript';
     script.src = url;
-    targetDoc.head.appendChild(script);
+    document.head.appendChild(script);
   }
 
-  var opened = false;
-
-  function openPopunder() {
-    if (opened) return;
-    opened = true;
-
-    var pop = window.open('about:blank', '_blank');
-    if (pop) {
-      pop.document.write('<!DOCTYPE html><html><head></head><body></body></html>');
-      pop.document.close();
-
-      // Đưa popunder ra sau
-      pop.blur();
-      window.focus();
-
-      // Tải script vào popunder (giữ nguyên URL gốc)
-      loadScript(pop.document, '//diagramjawlineunhappy.com/t/9/fret/meow4/1792720/e9692f9f.js');
-      loadScript(pop.document, '//diagramjawlineunhappy.com/t/9/fret/meow4/1767634/0b79b57a.js');
-    } else {
-      console.warn("Trình duyệt đã chặn popunder");
-    }
-  }
-
-  // Lắng nghe click ở bất kỳ đâu trên trang
-  document.addEventListener('click', openPopunder);
+  loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1792720/e9692f9f.js'); 
+  loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1767634/0b79b57a.js'); 
 })();
-</script>
