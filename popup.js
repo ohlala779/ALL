6635bc8734149b1ed4c7e37968f0e2b35fc806bb
ohlala779 <regdomainkris@gmail.com>
2025-08-14 06:54:43 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,28 +1,12 @@
-<script>
-(function() {
-  const key = 'popupClosedUntil';
-  const now = Date.now();
-  const delay = 5 * 60 * 1000; // 10 phút
-
+(function(){
   function loadScript(url) {
     var script = document.createElement('script');
-    script.setAttribute('data-cfasync', 'false');
     script.type = 'text/javascript';
     script.src = url;
+    script.onload = () => console.log("Loaded:", url);
+    script.onerror = () => console.error("Failed:", url);
     document.head.appendChild(script);
   }
-
-  // Kiểm tra có được phép load popup không
-  const closedUntil = localStorage.getItem(key);
-  if (!closedUntil || now > parseInt(closedUntil)) {
-    // Load các script popup
-    loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1792720/e9692f9f.js'); 
-    loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1767634/0b79b57a.js'); 
-
-    // Khi popup đóng thì set thời gian khoá (ví dụ sự kiện này do script ngoài bắn ra)
-    window.closePopupOnce = function() {
-      localStorage.setItem(key, now + delay);
-    };
-  }
+  loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1792720/e9692f9f.js'); 
+  loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1767634/0b79b57a.js'); 
 })();
-</script>
