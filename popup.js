39fddec44e447ea1081e5b419da8ea4c51860daa
ohlala779 <regdomainkris@gmail.com>
2025-08-14 06:45:31 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,23 +1,8 @@
 <script>
 (function() {
-  var cookieName = 'popupClosedUntil';
-  var delayMinutes = 5; // đổi thành 5 nếu muốn 5 phút
-
-  function setCookie(name, value, minutes) {
-    var d = new Date();
-    d.setTime(d.getTime() + (minutes*60*1000));
-    document.cookie = name + "=" + value + ";expires=" + d.toUTCString() + ";path=/";
-  }
-
-  function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-      var c = ca[i].trim();
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length);
-    }
-    return null;
-  }
+  const key = 'popupClosedUntil';
+  const now = Date.now();
+  const delay = 5 * 60 * 1000; // 10 phút
 
   function loadScript(url) {
     var script = document.createElement('script');
@@ -27,17 +12,16 @@
     document.head.appendChild(script);
   }
 
-  var closedUntil = getCookie(cookieName);
-  var now = Date.now();
-
+  // Kiểm tra có được phép load popup không
+  const closedUntil = localStorage.getItem(key);
   if (!closedUntil || now > parseInt(closedUntil)) {
-    // Load script popup
+    // Load các script popup
     loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1792720/e9692f9f.js'); 
     loadScript('//diagramjawlineunhappy.com/t/9/fret/meow4/1767634/0b79b57a.js'); 
 
-    // Hàm này sẽ được gọi khi người dùng đóng popup
+    // Khi popup đóng thì set thời gian khoá (ví dụ sự kiện này do script ngoài bắn ra)
     window.closePopupOnce = function() {
-      setCookie(cookieName, now + (delayMinutes * 60 * 1000), delayMinutes);
+      localStorage.setItem(key, now + delay);
     };
   }
 })();
